63d3dc8c2c4dcaeab7c8d57731425a9566e49b6b
artizanw <artizanw1@gmail.com>
2025-10-04 16:10:38 -0400

    feat!: restructure user config directory

--- himewm/src/directories.rs
+++ himewm/src/directories.rs
@@ -1,6 +1,7 @@
 use directories::BaseDirs;
 
 pub struct Directories {
+    pub himewm_dir: std::path::PathBuf,
     pub config_dir: std::path::PathBuf,
     pub layouts_dir: std::path::PathBuf,
 }
@@ -8,9 +9,11 @@ pub struct Directories {
 impl Directories {
     pub fn new() -> Self {
         let base_dirs = BaseDirs::new().unwrap();
-        let config_dir = base_dirs.config_dir().join("himewm");
-        let layouts_dir = config_dir.join("layouts");
+        let himewm_dir = base_dirs.config_dir().join("himewm");
+        let config_dir = himewm_dir.join("config");
+        let layouts_dir = himewm_dir.join("layouts");
         return Self {
+            himewm_dir,
             config_dir,
             layouts_dir,
         };
@@ -19,6 +22,7 @@ impl Directories {
 
 pub fn create_dirs() -> std::io::Result<()> {
     let dirs = Directories::new();
+    let _himewm_dir = std::fs::create_dir(dirs.himewm_dir)?;
     let _config_dir = std::fs::create_dir(dirs.config_dir)?;
     let _layouts_dir = std::fs::create_dir(dirs.layouts_dir)?;
     return Ok(());
